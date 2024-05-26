6f28a3384ad6ce1f4aa194667b02af26b57c0aab
lzy <lllzzzyyy@buaa.edu.cn>
2024-05-26 08:24:54 +0800

    学习到了 常用函数 system

---
+++ C16-并发/parallel/signal/anytimer/main.c
@@ -0,0 +1,40 @@
+#include "anytimer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void f1(void *p)
+{
+    printf("f1():%s\n", (char *)p);
+}
+
+static void f2(void *p)
+{
+    printf("f1():%s\n", (char *)p);
+}
+
+int main(int argc, char **argv)
+{
+    int job1;
+
+    puts("Begin!");
+
+    job1 = at_addjob(5, f1, "aaa");
+    if (job1 < 0)
+    {
+        fprintf(stderr, "at_addjob() failed!:%s\n", strerror(-job1));
+        exit(1);
+    }
+
+    puts("End!");
+
+    while (1)
+    {
+        write(1, ".", 1);
+        sleep(1);
+    }
+
+
+    exit(0);
+}
